20792b2ee3f4badca41275fb0bf8a088dc1ede42
Greg ORIOL <greg@gregoriol.net>
2017-12-11 13:49:54 +0100

    Moved IPChecker to a folder
    # Conflicts:
    #	StreamIt.xcodeproj/project.pbxproj

--- StreamIt/StreamIt-Bridging-Header.h
+++ StreamIt/StreamIt-Bridging-Header.h
@@ -2,10 +2,4 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#ifndef ip_addr_bridging_header_h
-#define ip_addr_bridging_header_h
-
-
-#endif /* ip_addr_bridging_header_h */
-
 #import "IPChecker.h"
